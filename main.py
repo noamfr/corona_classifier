a9da5a8a5a74adcc86372152a2e60d927a7841bb
noamfr <noam.frank1@gmail.com>
2020-08-04 00:49:07 +0300

    added pre analysis API class

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from pipeline.pipeline import Pipeline
-from configuration.config import Config
+
+SAVE_PROGRESS = True
+
 
 if __name__ == '__main__':
 
     pipeline = Pipeline()
 
     pipeline.get_data()
+    pipeline.preliminary_analysis()
     pipeline.prep_data()
     pipeline.basic_analysis()
     pipeline.run_classification()
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,18 +1,23 @@
 import pandas as pd
 from configuration.config import Config
 from data_layer.raw_data_api import Raw_Data_Api
+from pre_analysis.pre_analysis_api import Pre_Analysis_Api
 
 
 class Pipeline:
     def __init__(self):
-        self.raw_data = pd.DataFrame
+        self.raw_df = None
 
     def get_data(self):
         raw_data_api = Raw_Data_Api()
-        self.raw_data = raw_data_api.get_raw_data()
+        self.raw_df = raw_data_api.get_raw_data()
+
+    def preliminary_analysis(self):
+        pre_analysis_api = Pre_Analysis_Api(raw_df=self.raw_df)
+        pre_analysis_api.run_pre_analysis()
 
     def prep_data(self):
-        pass
+        print('data is prepped')
 
     def basic_analysis(self):
         pass
