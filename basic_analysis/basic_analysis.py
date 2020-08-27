341e606cf057048b2375fcbda04eb2e0ebe79712
noamfr <noam.frank1@gmail.com>
2020-08-28 01:28:58 +0300

    added binary data fields frequency analysis. added function for removing missing data values.

--- basic_analysis/basic_analysis.py
+++ basic_analysis/basic_analysis.py
@@ -1,20 +1,23 @@
 import os
 from statistics import mean
 import pandas as pd
-from collections import defaultdict
+from collections import defaultdict, Counter
 from config.config import Config
 from get_data.data_fields import Data_Fields
 from infra.graph_operations import bar_chart
 from infra.data_operations import remove_missing_values_from_array
+from .crosstab import Cross_Tab_Binary
 
 
 class Basic_Analysis:
     def __init__(self, patients):
         self.patients = patients
         self.local_output_path = os.path.join(Config.OUTPUT_PATH, 'basic_analysis')
+        self.binary_data_fields_arrays = None
 
     def calc(self):
         self.__binary_fields_frequency()
+        self.__binary_fields_by_target()
 
     def __binary_fields_frequency(self):
         binary_data_fields_arrays = {}
@@ -41,6 +44,7 @@ class Basic_Analysis:
             data_dict['negative_count'].append(negative_count)
             data_dict['missing_count'].append(missing_count)
 
+        self.binary_data_fields_arrays = binary_data_fields_arrays
         df = pd.DataFrame(data_dict)
         df.to_csv(os.path.join(self.local_output_path, 'binary_fields_frequency.csv'))
 
@@ -52,4 +56,46 @@ class Basic_Analysis:
                   output_path=self.local_output_path)
 
     def __binary_fields_by_target(self):
-        pass
+        report_dict = defaultdict(list)
+
+        target = Data_Fields.get_target()
+        binary_data_fields = Data_Fields.get_binary_vars()
+        binary_data_fields.remove(target)
+
+        target_array = [getattr(patient, target) for patient in self.patients]
+
+        for field in binary_data_fields:
+            vector = [getattr(patient, field) for patient in self.patients]
+            cross_tab = Cross_Tab_Binary(vector_1=target_array, vector_2=vector)
+
+            field_positive_corona_positive_count = cross_tab.get_cross_tab_count(v1_binary_value=1, v2_binary_value=1)
+            field_positive_corona_negative_count = cross_tab.get_cross_tab_count(v1_binary_value=0, v2_binary_value=1)
+            field_negative_corona_positive_count = cross_tab.get_cross_tab_count(v1_binary_value=1, v2_binary_value=0)
+            field_negative_corona_negative_count = cross_tab.get_cross_tab_count(v1_binary_value=0, v2_binary_value=0)
+            missing_values_count = cross_tab.v2_missing_count
+
+            field_positive_corona_positive_percent = cross_tab.get_cross_tab_percent(v1_binary_value=1, v2_binary_value=1)
+            field_positive_corona_negative_percent = cross_tab.get_cross_tab_percent(v1_binary_value=0, v2_binary_value=1)
+            field_negative_corona_positive_percent = cross_tab.get_cross_tab_percent(v1_binary_value=1, v2_binary_value=0)
+            field_negative_corona_negative_percent = cross_tab.get_cross_tab_percent(v1_binary_value=0, v2_binary_value=0)
+
+            corona_positive_percent_for_field_positives = cross_tab.get_perc_v1_positive_from_all_v2_positives()
+            corona_positive_percent_for_field_negatives = cross_tab.get_perc_v1_positive_from_all_v2_negatives()
+
+            report_dict['field'].append(field)
+            report_dict['field_pos_corona_pos_count'].append(field_positive_corona_positive_count)
+            report_dict['field_pos_corona_neg_count'].append(field_positive_corona_negative_count)
+            report_dict['field_neg_corona_pos_count'].append(field_negative_corona_positive_count)
+            report_dict['field_neg_corona_neg_count'].append(field_negative_corona_negative_count)
+            report_dict['missing_values_count'].append(missing_values_count)
+
+            report_dict['field_pos_corona_pos_percent'].append(field_positive_corona_positive_percent)
+            report_dict['field_pos_corona_neg_percent'].append(field_positive_corona_negative_percent)
+            report_dict['field_neg_corona_pos_percent'].append(field_negative_corona_positive_percent)
+            report_dict['field_neg_corona_neg_percent'].append(field_negative_corona_negative_percent)
+
+            report_dict['corona_pos_percent_for_field_positives'].append(corona_positive_percent_for_field_positives)
+            report_dict['corona_pos_percent_for_field_negatives'].append(corona_positive_percent_for_field_negatives)
+
+        df = pd.DataFrame(report_dict)
+        df.to_csv(os.path.join(self.local_output_path, 'binary_fields_by_target.csv'))
