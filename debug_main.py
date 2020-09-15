7ab024acf5073e4fc5f9a2b84fe3ec5480c1e91b
noamfr <noam.frank1@gmail.com>
2020-09-16 00:06:08 +0300

    clean up

--- debug_main.py
+++ debug_main.py
@@ -1,9 +1,10 @@
 from pipeline.pipeline import Pipeline
-from infra.pipeline_executor import Pipeline_Executor
+from pipeline_operations.pipeline_executor import Pipeline_Executor
 from config.config import Config
 
 SAVE_PROGRESS = True
 JOB_KEY = 'corona_classifier'
+CONFIG = Config()
 
 if __name__ == '__main__':
 
@@ -13,7 +14,7 @@ if __name__ == '__main__':
         'get_data',
         'treat_missing_values',
         'prep_data',
-        'basic_analysis',
+        'exploratory_analysis',
         'run_classification'
     ]
 
