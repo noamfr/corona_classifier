a5df16d217cf0919165a1eef508ca492805b13e1
noamfr <noam.frank1@gmail.com>
2020-10-10 23:27:47 +0300

    Added more configurations

--- config/config.py
+++ config/config.py
@@ -1,5 +1,9 @@
-import os
-from .static_configs import Static_Configs
+from os import path, listdir
+import yaml
+from typing import Dict, List
+
+from .static_values import Static_Configs
+from .vitals_values import Vital_Values, Vitals_Container
 
 
 class Config:
@@ -7,15 +11,93 @@ class Config:
     RAW_DATA_PATH: str
     PICKLE_PATH: str
     OUTPUT_PATH: str
+    ADULT_AGE_THRESHOLD: int
+    ACCEPTED_VALUES_YAML_PATH: str
+    CHILDREN_HEALTHY_LOWER: Dict
+    CHILDREN_HEALTHY_UPPER: Dict
+    VITALS_CONTAINER: Vitals_Container
 
-    WORK_DIR = 'C:/Users/normy/corona_classifier_files'
-    RAW_DATA_PATH = 'C:/Users/normy/PycharmProjects/covidclinicaldata/data'
-    PICKLE_PATH = os.path.join(WORK_DIR, 'pickle_files')
-    OUTPUT_PATH = os.path.join(WORK_DIR, 'outputs')
+    def __init__(self):
+
+        __class__.WORK_DIR = 'C:/Users/normy/corona_classifier_files'
+        __class__.RAW_DATA_PATH = 'C:/Users/normy/PycharmProjects/covidclinicaldata/data'
+        __class__.PICKLE_PATH = path.join(Config.WORK_DIR, 'pickle_files')
+        __class__.OUTPUT_PATH = path.join(Config.WORK_DIR, 'outputs')
+        __class__.ADULT_AGE_THRESHOLD = Static_Configs.ADULT_AGE_THRESHOLD
+        __class__.ACCEPTED_VALUES_YAML_PATH = 'C:/Users/normy/PycharmProjects/corona_classifier/config/' \
+                                              'accepted_values_yamls'
+
+        # __class__.CHILDREN_HEALTHY_LOWER = self.load_vitals(file_name='children_healthy_lower')
+        # __class__.CHILDREN_HEALTHY_UPPER = self.load_vitals(file_name='children_healthy_upper')
+        # __class__.ADULTS_HEALTHY_LOWER = self.load_vitals(file_name='adults_healthy_lower')
+        # __class__.ADULTS_HEALTHY_UPPER = self.load_vitals(file_name='adults_healthy_upper')
+
+        __class__.VITALS_CONTAINER = self.build_vitals_container()
 
     @classmethod
     def get_missing_value_threshold(cls):
         return Static_Configs.MISSING_VALUES_THRESHOLD
+
+    @staticmethod
+    def load_yaml(file_name):
+        with open(path.join(Config.ACCEPTED_VALUES_YAML_PATH, f'{file_name}'), 'r') as data:
+            data_loaded = yaml.safe_load(data)
+        return data_loaded
+
+    def load_vitals(self, file_name):
+        vitals = self.load_yaml(file_name)
+        vital_values = Vital_Values(population=vitals['population'],
+                                    threshold_category=vitals['threshold_category'],
+                                    threshold_type=vitals['threshold_type'],
+                                    temperature=vitals['temperature'],
+                                    pulse=vitals['pulse'],
+                                    sys=vitals['sys'],
+                                    dia=vitals['dia'],
+                                    rr=vitals['rr'],
+                                    sats=vitals['sats'])
+
+        return vital_values
+
+    def build_vitals_container(self):
+        vitals_container = Vitals_Container()
+        for file_name in listdir(Config.ACCEPTED_VALUES_YAML_PATH):
+            vitals = self.load_vitals(file_name)
+            setattr(vitals_container, file_name, vitals)
+            vitals_container.vitals_list.append(vitals)
+
+        return vitals_container
+
+
+
+
+
+
+
+    # def __load__vitals_thresholds_from_yaml(self, population, threshold_category, threshold_type):
+    #     vitals_threshold_map = defaultdict[lambda: defaultdict(dict)]
+    #     for file_name in listdir(path.join(Config.ACCEPTED_VALUES_YAML_PATH,
+    #                                        population, threshold_category, threshold_type)):
+    #
+    #         vitals = self.load_yaml(file_name)
+    #         vitals_threshold_map[population][threshold_category][threshold_type] = vitals
+    #
+    #     #
+    #     # for population in [Population.CHILDREN, Population.ADULTS]:
+    #     #     for threshold_category in [Threshold_Category.HEALTHY_VALUES, Threshold_Category.ACCEPTED_VALUES]:
+    #     #         for threshold_type in [Threshold_Type.LOWER, Threshold_Type.UPPER]:
+    #     #             for file_name in listdir(path.join(Config.ACCEPTED_VALUES_YAML_PATH, population)):
+    #     #                 vitals = self.load_yaml(file_name)
+        #                 if population == Population.CHILDREN:
+        #                     __class__.CHILDREN_VITALS = {}
+        #                     __class__.CHILDREN_VITALS['lower] = Accepted_Value_Map(temperature=vitals['temperature'],
+        #                                                                    pulse=vitals['pulse'],
+        #                                                                    sys=vitals['sys'],
+        #                                                                    dia=vitals['dia'],
+        #                                                                    rr=vitals['rr'],
+        #                                                                    sats=vitals['sats'])
+        #
+        #
+
 
 
 
