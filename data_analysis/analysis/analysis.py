b1ed01e87526b4ad21bfcd1a1d2c58c24010875c
noamfr <noam.frank1@gmail.com>
2020-11-03 23:38:13 +0200

    new structure working

--- data_analysis/analysis/analysis.py
+++ data_analysis/analysis/analysis.py
@@ -10,29 +10,32 @@ from .missing_values_analysis.missing_values_analysis import Missing_Values_Anal
 
 class Analysis:
     def __init__(self, data: Data):
-        self.__patients = data.patients
+        self.__patients = data.get_patients
+        self.__vectors = data.get_vectors
         self.__report_tables: Dict = {}
-        self.__graph_vectors: Dict[str: ndarray] = {}
+        self.__output_graph_vectors: Dict[str: ndarray] = {}
 
         self.__calc()
 
     def __calc(self):
         self.__missing_values_analysis()
         self.__binary_fields_analysis()
-        # self.__continuous_fields_analysis()
+        self.__continuous_fields_analysis()
         # self.__age_analysis()
 
     def __missing_values_analysis(self):
         missing_values_analysis = Missing_Values_Analysis(patients=self.__patients)
         self.__report_tables = {**self.__report_tables, **missing_values_analysis.get_report_tables}
-        self.__graph_vectors = {**self.__graph_vectors, **missing_values_analysis.get_graph_vectors}
+        self.__output_graph_vectors = {**self.__output_graph_vectors, **missing_values_analysis.get_graph_vectors}
 
     def __binary_fields_analysis(self):
-        binary_fields_analysis = Binary_Fields_Analysis(patients=self.__patients)
+        binary_fields_analysis = Binary_Fields_Analysis(patients=self.__patients, vectors=self.__vectors)
         self.__report_tables = {**self.__report_tables, **binary_fields_analysis.get_report_tables}
 
     def __continuous_fields_analysis(self):
-        continuous_fields_analysis = Continuous_Fields_Analysis(patients=self.__patients)
+        continuous_fields_analysis = Continuous_Fields_Analysis(patients=self.__patients, vectors=self.__vectors)
+        self.__report_tables = {**self.__report_tables, **continuous_fields_analysis.get_report_tables}
+        self.__output_graph_vectors = {**self.__output_graph_vectors, **continuous_fields_analysis.get_graph_vectors}
 
     def __age_analysis(self):
         age_analysis = Age_Analysis(patients=self.__patients, publish_results=True)
@@ -44,4 +47,4 @@ class Analysis:
 
     @property
     def get_graph_vectors(self):
-        return self.__graph_vectors
+        return self.__output_graph_vectors
